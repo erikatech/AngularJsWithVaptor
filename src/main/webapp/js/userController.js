62686a12185d0b9b45b75db800ffbc4b80ff82a5
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-07-25 01:41:07 -0300

    fixing bug

--- src/main/webapp/js/userController.js
+++ src/main/webapp/js/userController.js
@@ -26,24 +26,24 @@ function userController($scope, $window, $http) {
     };
     
     $scope.deleteUser = function(user){
-		var confirm = $window.confirm('Remove user ' + user.login + '?');
-		if(confirm){
-			var url = '/angularJs/users/' + user.id;
+	var confirm = $window.confirm('Remove user ' + user.login + '?');
+	if(confirm){
+		var url = '/angularJs/users/' + user.id;
 
-			$http.delete(url).success(function(data){
-				var index = $scope.users.indexOf(user);
-				$scope.users.splice(index, 1);
-			});
-		}
-	};
+		$http.delete(url).success(function(data){
+			var index = $scope.users.indexOf(user);
+			$scope.users.splice(index, 1);
+		});
+	}
+    };
 	
-	$scope.edit = function(user){
-		$scope.user = user;
-		$(".btn-success").attr("disabled", "disabled");
-		$(".btn-primary").removeAttr("disabled");
-	};
+    $scope.edit = function(user){
+	$scope.user = user;
+	$(".btn-success").attr("disabled", "disabled");
+	$(".btn-primary").removeAttr("disabled");
+    };
 	
-	var reset = function(){
-		$scope.user = {id : 0, login : "", name : "", password: ""};
-	};
+    var reset = function(){
+	$scope.user = {id : 0, login : "", name : "", password: ""};
+    };
 }
